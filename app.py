447b4b71ec82fae34371df05db6076fff0548510
Lux Tech Academy <51136314+HarunHM@users.noreply.github.com>
2020-08-15 03:04:44 +0300

    Updated app.py
    Added Your PassWord as the placeholder for my password

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '33812040'
+app.config['MYSQL_PASSWORD'] = 'Your PassWord'
 app.config['MYSQL_DB'] = 'MyDB'
 
 mysql = MySQL(app)
